140b72a0a9166790baaa4882ed31f6adafdc2253
Ken-Smit <builtwithhandz@gmail.com>
2024-12-07 19:24:11 -0600

    initial commit

--- src/DataReader.java
+++ src/DataReader.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+/*Kenneth Smith
+420 Module 1
+10-25-24
+*/
+
+//import classes required for this program
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class DataReader {
+    public static void main(String[] args) {
+        String filename = "KennethSmith.dat";
+        try (DataInputStream in = new DataInputStream(new FileInputStream(filename))) {
+
+            // Reading five random integers
+            int intArrayLength = in.readInt();
+            int[] intArray = new int[intArrayLength];
+            System.out.println("5 Random Integers: ");
+            for (int i = 0; i < intArrayLength; i++) {
+                intArray[i] = in.readInt();
+                System.out.print(intArray[i] + " ");
+            }
+            System.out.println();
+
+            // Reading current date
+            long dateMilliseconds = in.readLong();
+            Date currentDate = new Date(dateMilliseconds);
+            System.out.println("Current Date: " + currentDate);
+
+            // Reading five random double values
+            int doubleArrayLength = in.readInt();
+            double[] doubleArray = new double[doubleArrayLength];
+            System.out.println("5 Random Double Values: ");
+            for (int i = 0; i < doubleArrayLength; i++) {
+                doubleArray[i] = in.readDouble();
+                System.out.print(doubleArray[i] + " ");
+            }
+            System.out.println();
+
+            //Display error message if program doesn't read successfully
+        } catch (IOException e) {
+            System.out.println("An error occurred check if the file exists: " + e.getMessage());
+        }
+    }
 }
--- src/DataWriter.java
+++ src/DataWriter.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+/*Kenneth Smith
+420 Module 1
+10-25-24
+*/
+
+//import classes necessary for program
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Random;
 
 public class DataWriter {
+    public static void main(String[] args) {
+        String filename = "KennethSmith.dat"; //Create file name
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream(filename))) {
+            Random random = new Random();
+
+            // Writing five random integers
+            out.writeInt(5); // Write the length of the integer array
+            for (int i = 0; i < 5; i++) {
+                out.writeInt(random.nextInt(1000)); // Random integer between 0 and 999
+            }
+
+            // Writing current date
+            Date currentDate = new Date();
+            out.writeLong(currentDate.getTime()); // Store date as long (milliseconds)
+
+            // Writing five random double values
+            out.writeInt(5); // Write the length of the double array
+            for (int i = 0; i < 5; i++) {
+                out.writeDouble(random.nextDouble() * 1000); // Random double between 0.0 and 1000.0
+            }
+
+            //Write all the contents into one file
+            System.out.println("Data written to file successfully.");
+            //Display error message if the file doesn't write successfully
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 }
